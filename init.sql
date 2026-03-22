836350ad360887415e0f4b37393ca901286a0ce6
Viet Tran <hello@viet.vn>
2026-03-22 15:57:46 +0700

    update

--- init.sql
+++ init.sql
@@ -3,8 +3,3 @@ CREATE TABLE IF NOT EXISTS products (
   name VARCHAR(255) NOT NULL,
   qty INTEGER NOT NULL DEFAULT 0
 );
-
-INSERT INTO products (name, qty) VALUES
-  ('Laptop', 15),
-  ('Mouse', 50),
-  ('Keyboard', 30);
