f77e3f8625821608857cf4d16ffb96ac4d4e4fec
Viet Tran <hello@viet.vn>
2026-04-09 16:57:40 +0700

    update

---
+++ sqs/1-std-send.mjs
@@ -0,0 +1,21 @@
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
+
+const QUEUE_URL =
+  process.env.QUEUE_URL ||
+  'https://sqs.ap-southeast-1.amazonaws.com/916495840179/vietaws';
+
+const sqsClient = new SQSClient({
+  region: process.env.REGION || 'ap-southeast-1',
+});
+
+for (let i = 0; i < 1000; i++) {
+  await sqsClient.send(
+    new SendMessageCommand({
+      QueueUrl: QUEUE_URL,
+      MessageBody: `Here is message #${i + 1}!`,
+    })
+  );
+
+  console.log(`Sent message #${i + 1} successfully!`);
+  await new Promise((resolver) => setTimeout(resolver, 500));
+}
---
+++ sqs/3-fifo-send.mjs
@@ -0,0 +1,23 @@
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
+
+const QUEUE_URL =
+  process.env.QUEUE_URL ||
+  'https://sqs.ap-southeast-1.amazonaws.com/916495840179/vietaws.fifo';
+
+const sqsClient = new SQSClient({
+  region: process.env.REGION || 'ap-southeast-1',
+});
+for (let i = 0; i < 1000; i++) {
+  const random = Math.floor(Math.random() * 2 + 1);
+  await sqsClient.send(
+    new SendMessageCommand({
+      QueueUrl: QUEUE_URL,
+      MessageBody: `Message  #${i + 1}!`,
+      MessageDeduplicationId: String(Math.random()),
+      MessageGroupId: `vietaws`,
+    })
+  );
+
+  console.log(`Sent message #${i + 1} successfully!`);
+  await new Promise((resolver) => setTimeout(resolver, 500));
+}
