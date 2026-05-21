5ecd7a75ee83a66d01e289959c857e1f40c6e070
Viet Tran <hello@viet.vn>
2026-05-21 14:17:41 +0700

    update

--- scripts/4-start_server.sh
+++ scripts/4-start_server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "==> [ApplicationStart] Starting New Application Server..."
-cd /home/ec2-user/architecting/app
-nohup node index.js > /var/log/app.log 2>&1 &
-sleep 2
-pgrep -f "node index.js" > /dev/null || exit 1
+systemctl restart demo-app
 echo "New Application Server Started successfully."
 exit 0
