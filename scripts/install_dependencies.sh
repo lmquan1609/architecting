f6ea107d032cf18da3c8adb64b145157a2df44bd
Viet Tran <hello@viet.vn>
2026-05-21 08:38:36 +0700

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/app
+cd /home/ec2-user/architecting/app
 npm install --production
 exit 0
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/app
+cd /home/ec2-user/architecting/app
 nohup node index.js > /var/log/app.log 2>&1 &
 sleep 2
 pgrep -f "node index.js" > /dev/null || exit 1
