4dd4be54250384304c60be434267ac1fb2eaa806
Viet Tran <hello@viet.vn>
2026-05-21 11:19:01 +0700

    v10

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const PORT = 3001;
 app.get('/', (req, res) => {
   res.send(`
     <body style='background-color: #283E5B; text-align: center;color: orange;'>
-      <h2>Hello from AWS Architecting Lab - v3</h2>
+      <h2>Hello from AWS Architecting Lab - v1.0</h2>
     <body>
   `);
 });
