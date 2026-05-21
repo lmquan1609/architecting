ada6d2bba36581c2974ac2e6930f6e1f3a0c7ee8
Viet Tran <hello@viet.vn>
2026-05-21 10:08:56 +0700

    update

--- scripts/2-before_install.sh
+++ scripts/2-before_install.sh
@@ -2,7 +2,6 @@
 
 # delete old node modules to ensure we have a clean slate for the new deployment
 rm -rf /home/ec2-user/architecting/app/node_modules
-
 # install new dependencies
 cd /home/ec2-user/architecting/app
 npm install --production
