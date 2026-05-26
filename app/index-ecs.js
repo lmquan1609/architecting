8c68c2af60d002c614a2285bc33b8c624da357f1
Viet Tran <hello@viet.vn>
2026-05-26 15:12:22 +0700

    Viet Tran - hello@viet.vn

--- app/index-ecs.js
+++ app/index-ecs.js
@@ -24,7 +24,7 @@ app.get("/", async (req, res) => {
     const containerName = os.hostname();
     console.log("os hostname: ", os.hostname());
     const ip = containerIp.split(":")[3];
-    const version = 3;
+    const version = 4;
     const bgColor = BACKGROUND_COLOR;
     const html = `
     <html>
