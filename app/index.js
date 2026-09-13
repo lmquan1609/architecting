de4b0475bc529a6cdcaeb6cde452f9deee788487
Quan Luu <quan.luu@timo.vn>
2026-09-13 13:57:08 +0700

    chore: upgrade to v5

--- app/index.js
+++ app/index.js
@@ -24,7 +24,7 @@ app.get("/", async (req, res) => {
     const containerName = os.hostname();
     console.log("os hostname: ", os.hostname());
     const ip = containerIp.split(":")[3];
-    const version = 5;
+    const version = 6;
     const bgColor = BACKGROUND_COLOR;
     const html = `
     <html>
